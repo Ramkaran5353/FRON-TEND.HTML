fdd3de56d243815e29aea77c4a6661cb65414ca2
Ram Karan <ram674781@gmail.com>
2024-09-17 10:15:48 +0530

    HTML project

---
+++ script.js
@@ -0,0 +1,16 @@
+let Firstname ='Ram'
+const faltumessage = 'hey im ramu'
+let Lastname ='karan'
+let imhappy = false
+let age= 20
+alert("your massage is here")
+confirm("your massage is here")
+let username = 'Hi, my name is'+ ' ' + 
+Firstname +' '+ 
+Lastname +' '+
+ '. im'+' '+ 
+ age +' '+
+  'years' +' '+ 
+  'old.'+' '+ 
+  'im happy.' +' '+
+   imhappy
